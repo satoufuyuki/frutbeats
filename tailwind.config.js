0f8b66e06f0e74f2f95915081d2d855c4caf1243
ZenShibata <zen@ayato.life>
2023-05-26 21:03:21 +0700

    feat: update screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        lg: "1320px"
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
